d392ac0831f7b5a8939eb9e431f0a4b4007c5dfc
nicholas <nicholasvpinheiro@gmail.com>
2025-03-15 13:58:18 -0300

    add cookies

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
 from flask import Flask, request, jsonify
 import yt_dlp
+import os
 
 app = Flask(__name__)
 
 def baixar_audio(url):
+    cookies_data = os.getenv("COOKIES_DATA")
+    
+    if not cookies_data:
+        raise ValueError("Erro: Variável de ambiente 'COOKIES_DATA' não encontrada.")
+    
+    with open("cookies.txt", "w", encoding="utf-8") as f:
+        f.write(cookies_data)
+
     ydl_opts = {
         'format': 'bestaudio/best',
         'postprocessors': [{
@@ -12,11 +21,13 @@ def baixar_audio(url):
             'preferredquality': '192',
         }],
         'outtmpl': '%(title)s.%(ext)s',
-        'cookiefile': 'cookies.txt'
+        'cookiefile': 'cookies.txt'  
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
+
+    os.remove("cookies.txt")
 
 @app.route('/download_audio', methods=['POST'])
 def download_audio():
